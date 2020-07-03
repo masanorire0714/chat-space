8169d2a18ad0a28aede10463a9c745134688c513
masanorire0714 <masanorire0714@gmail.com>
2020-07-03 16:08:31 +0900

    asynchronous

--- Gemfile
+++ Gemfile
@@ -65,3 +65,4 @@ group :development, :test do
   gem 'factory_bot_rails'
   gem 'faker'
 end
+gem 'jquery-rails'
---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,72 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="MessageField">
+          <div class="MessageBox">
+            <div class="message-info">
+             <div class="message-info__name">
+                ${message.user_name}
+              </div>
+              <div class="message-info__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="message-text">
+              <div class="message-text__main">
+                <p class="Message__content">
+                  ${message.content}
+                </p>
+              </div>
+            </div>
+          </div>
+         </div>`
+       return html;
+     } else {
+      let html =
+      `<div class="MessageField">
+      <div class="MessageBox">
+        <div class="message-info">
+          <div class="message-info__name">
+            ${message.user_name}
+          </div>
+          <div class="message-info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message-text">
+          <div class="message-text__main">
+            <p class="Message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>`
+    return html;
+  };
+}
+
+  $('Form').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this)
+    let url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.MessageField').append(html);
+      $('.list-message').animate({ scrollTop: $('.list-message')[0].scrollHeight});
+      $('form')[0].reset();
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+  });
+});
